73d8b609095e363ee12edd7545ebce93b12eada5
mayoyooyo <ittiwat.35131@gmail.com>
2021-02-17 17:48:53 +0700

    lock

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@ from flask import Flask, request,jsonify
 from flask_pymongo import PyMongo 
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = ''
+app.config['MONGO_URI'] = 'mongodb://exceed_group17:9pd9akda@158.108.182.0:2255/exceed_group17'
 mongo = PyMongo(app)
 
-myCollection = mongo.db.
+myCollection = mongo.db.test
 
 @app.route('/create', methods = ['POST']) #create user
 def create_user():
@@ -15,15 +15,15 @@ def create_user():
         'user': data['user'],
         'name': data['name']
     }
-    user_name = data['user']
-    Duplicate = myCollection.find(user_name)
+    user_name = {'user': data['user']}
+    cursor = myCollection.find_one(user_name)
+    results = list(cursor)
 
-    if not Duplicate:
+    if len(results) == 0:
         myCollection.insert_one(data_insert)
         return {'result' : 'create successful'}
     else:
         return {'result' : 'this user already create'}
-
 
 @app.route('/check', methods = ['GET'])
 def check_user():
@@ -36,6 +36,15 @@ def check_user():
     else:
         return {'result': 'the user name does not exist'}
 
+@app.route('/status_locker', methods = ['GET'])
+def locker_status():
+    data = request.json
+    status = data['lock']
+
+    if status == True:
+        return {'result': 'lock'}
+    else:
+        return {'result': 'unlock'}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
