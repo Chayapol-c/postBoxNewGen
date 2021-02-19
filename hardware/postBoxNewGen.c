544e7e279d183358d1dc8f65dadf2847cc830f4d
OPALNAKUUUB <makara.atipat@gmail.com>
2021-02-19 21:49:33 +0700

    finish_hardware_V01

--- hardware/postBoxNewGen.c
+++ hardware/postBoxNewGen.c
@@ -5,8 +5,14 @@
 
 const char* ssid = "Opalnakuuub";
 const char* password = "123456789";
+char str[100];
 
-const char* url = "http://158.108.182.23:3000";
+const int _size = 2 * JSON_OBJECT_SIZE(2);
+StaticJsonDocument<_size> JSONGet;
+StaticJsonDocument<_size> JSONPost;
+
+const char* url_get = "http://158.108.182.23:3000/status?user=yooo";
+const char* url_post = "http://158.108.182.23:3000/status/update";
 
 Servo servo_user;
 Servo servo_postman;
@@ -29,21 +35,25 @@ void WiFi_Connect() {
 }
 
 void _get() {
-    if(Wifi.status() == WL_CONNECTED) {
+    if(WiFi.status() == WL_CONNECTED) {
         HTTPClient http;
-        http.begin(url);
+        http.begin(url_get);
         int httpCode = http.GET();
         if(httpCode == HTTP_CODE_OK) {
-            Serial.println(httpCode);
-            Serial.print("Lock_user: ");
-            Serial.println((bool)JSONGet["Lock_user"]);
-            get_user = JSONGet["Lock_user"];
-            Serial.print("Lock_postman: ");
-            Serial.println((bool)JSONGet["Lock_postman"]);
-            get_postman = JSONGet["Lock_postman"];
-        }else {
-            Serial.println(httpCode);
-            Serial.println("ERROR on HTTP Request");
+            String payload = http.getString();
+            DeserializationError err = deserializeJson(JSONGet, payload);
+            if(!err) {
+                Serial.println(httpCode);
+                Serial.print("Lock_user: ");
+                Serial.println((bool)JSONGet["Lock_user"]);
+                get_user = JSONGet["Lock_user"];
+                Serial.print("Lock_postman: ");
+                Serial.println((bool)JSONGet["Lock_postman"]);
+                get_postman = JSONGet["Lock_postman"];
+            }else {
+                Serial.println(httpCode);
+                Serial.println("ERROR on HTTP Request");
+            }
         }
     }else {
         WiFi_Connect();
@@ -51,16 +61,20 @@ void _get() {
 }
 
 void _post(int sw, bool status) {
-    if(WiFi.status() == WL_CONECTED) {
-        HTTPCLient http;
-        http.begin(url);
+    if(WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+        http.begin(url_post);
         http.addHeader("Content-Type", "application/json");
         if(sw == 0) {
-            JSONPost["Lock_user"] = state;
+            JSONPost["username"] = "yooo";
+            JSONPost["Lock_user"] = status;
+            JSONPost["Lock_postman"] = get_postman;
         }else {
-            JSONPost["Lock_postman"] = state;
+            JSONPost["username"] = "yooo";
+            JSONPost["Lock_postman"] = status;
+            JSONPost["Lock_user"] = get_user;
         }
-        
+        Serial.println(str);
         serializeJson(JSONPost, str);
         int httpCode = http.POST(str);
 
@@ -77,38 +91,39 @@ void _post(int sw, bool status) {
     }
 }
 
+
 void setup() {
     servo_user.attach(sv_user);
     servo_postman.attach(sv_postman);
+    servo_user.write(90);
+    servo_postman.write(90);
     Serial.begin(9600);
     delay(4000);
     WiFi_Connect();
 }
 
 void loop() {
-    _get();// Lock_user, Lock_postman
-    //Serial.println(analogRead(ldr_postman), DEC);
-    //delay(1000);
-
-    // USER
-    if(analogRead(ldr_user) > 600) {
+    _get();
+    Serial.println(analogRead(ldr_postman), DEC);
+    delay(1000);
+    // POSTMAN
+    if(!get_postman) {
+        servo_postman.write(0);
+    }
+    if(analogRead(ldr_postman) > 2500) {
+        servo_postman.write(90);//LOCK
+        delay(500);
+        _post(1, true);
+    }
+    
+    //USER
+    if(!get_user) {
+        servo_user.write(0);
+    }
+    if(analogRead(ldr_user) > 2500) {
         servo_user.write(90); // lock
         delay(500);
         _post(0, true);
-        
-    }else {
-        servo_user.write(0);
-        delay(500);
-        _post(0, false);
     }
-    // POSTMAN
-    if(analogRead(ldr_postman) > 600) {
-        servo_postman.write(90);
-        delay(500);
-        _post(1, true);
-    }else {
-        servo_postman.write(0);
-        delay(500);
-        _post(1, false);
-    }
+    delay(5000);
 }
