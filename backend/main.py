5e9f4f49dff18925aed27b2fdb99047ca5bddd03
mayoyooyo <ittiwat.35131@gmail.com>
2021-02-19 19:46:26 +0700

    add post man track

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ from flask_pymongo import PyMongo
 from os import path, environ
 from os.path import join, dirname
 from dotenv import load_dotenv
+from datetime import datetime
+from time import time
+from flask_cors import CORS
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -13,6 +16,14 @@ mongo = PyMongo(app)
 
 userMongodb = mongo.db.user
 trackMongodb = mongo.db.track
+lockerUserMongodb = mongo.db.locker_status_postman
+lockerPostmanMongodb = mongo.db.locker_status_user
+
+def timestamp():
+    fmt = '%Y-%m-%d %H:%M:%S'
+    ts = time()
+    currentTime = str(datetime.fromtimestamp(ts).strftime(fmt))
+    return currentTime
 
 @app.route('/create', methods = ['POST']) #create user
 def create_user():
@@ -36,7 +47,7 @@ def create_user():
         userMongodb.insert_one(data_insert)
         return {'result' : 'create successful'}
     else:
-        return {'result' : 'this user already create'}
+        return {'result' : 'this user already exist'}
 
 @app.route('/login', methods = ['GET'])  #login
 def check_user():
@@ -70,34 +81,66 @@ def locker_status():
     else:
         return {'result': 'unlock'}
 
-@app.route('/user_add_track', methods = ['POST'])
+@app.route('/user_add_track', methods = ['POST'])  #user add trackID
 def add_track():
     data = request.json
     
     data_insert = {
-        'user': data['user'],
+        'username': data['username'],
         'name': data['name'],
-        'trackID': data['trackID']
+        'trackID': data['trackID'],
+        'timestamp': 0
     }
 
-    user_name = {'user': data['user']}
+    user_name = {'username': data['username']}
     cursor = userMongodb.find(user_name)
 
     output = []
     for ele in cursor:
         output = {
-            'user': ele['user'],
-            'name': data['name'],
-            'trackID': ele['trackID']
+            'username': ele['username'],
         }
 
-    if len(output) == 0:
+    if len(output) == 1:
         trackMongodb.insert_one(data_insert)
         return {'result' : 'add track successful'}
     else:
-        return {'result' : 'unknow user'}
+        return {'result' : 'unknown user'}
 
-@app.route('/postman_track', methods = ['GET'])
+@app.route('/postman_track', methods = ['PATCH'])
+def postman_track():
+    data = request.json
 
+    filt = {
+        'trackID': data['trackID']
+    }    
+
+    cursor = trackMongodb.find(filt)
+
+    output_user = []
+    output_track = []
+    for ele in cursor:
+        output_track = {
+            'trackID': ele['trackID'],
+            'timestamp': ele['timestamp']
+        }
+        output_user = {
+            'username': ele['username']
+        }
+
+    lockerUser = {'username': output_user['username']}
+
+    update_track = {'$set': {'timestamp': timestamp()}}
+    update_locker = {'$set': {'Lock_postman': False}}
+
+    if len(output_track) == 0:
+        return {'result': 'invalid trackID'}
+    else:
+        if(output_track['timestamp'] != 0):
+            return {'result': 'this track already sent'}
+        trackMongodb.update_one(filt, update_track)
+        lockerPostmanMongodb.update_one(lockerUser, update_locker)
+        return {'result': 'sent!'}
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
