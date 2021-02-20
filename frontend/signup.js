5839b93388eeda7b77761dfb385c72fdfbc62664
Chayapol-c <chayapol.cha@ku.th>
2021-02-21 03:45:04 +0700

    add files

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,10 +1,40 @@
 "use-strict"
-
 const signupbtn = document.querySelector("#signup--btn");
+const login = document.querySelector("#login")
 
 signupbtn.addEventListener("click", function(e){
     e.preventDefault()
     const username = signupbtn.closest("#user-form").querySelector("#InputUsername1").value
     const password =signupbtn.closest("#user-form").querySelector("#InputPassword1").value
-    console.log(username, password)
+    
+    if (username && password){
+      const userData ={
+          "username": username,
+          "password": password
+      }
+  
+      fetch("http://158.108.182.23:3001/create", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => console.log(err))
+      window.location.href = "landing_page.html"
+    }
+    
+
+})
+
+login.addEventListener("mouseover", function(){
+  this.style.cursor= "pointer"
+})
+
+login.addEventListener("click", function(){
+  window.location.href = "login.html"
 })
